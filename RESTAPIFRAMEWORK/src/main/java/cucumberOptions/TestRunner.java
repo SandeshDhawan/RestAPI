8c0df58427008f26fe8acf76f1f67596dc89c2aa
Sandesh Dhawan <sdhawan12494@gmail.com>
2020-08-24 17:02:34 +0530

    Updated Test Runner

--- RESTAPIFRAMEWORK/src/main/java/cucumberOptions/TestRunner.java
+++ RESTAPIFRAMEWORK/src/main/java/cucumberOptions/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 //@RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/Postman_TestCases", plugin = "json:target/jsonReports/cucumber-report.json", glue = "StepDefinations")
+@CucumberOptions(features = "src/test/java/TestCases", plugin = "json:target/jsonReports/cucumber-report.json", glue = "StepDefinations")
 public class TestRunner extends AbstractTestNGCucumberTests {
 
 }
